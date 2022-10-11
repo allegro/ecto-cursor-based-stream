a9e6153f10ce3050e3f290e0e6737a3f1f9874b9
Jacek Tomaszewski <jacek.a.tomaszewski@allegro.pl>
2022-10-11 09:30:30 +0200

    Add test GH workflow

--- mix.exs
+++ mix.exs
@@ -11,18 +11,16 @@ defmodule EctoCursorBasedStream.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 end
