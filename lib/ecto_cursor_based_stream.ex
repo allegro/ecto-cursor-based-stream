e12a56d2e091633a01449450e5e585d182bfeaf5
Jacek Tomaszewski <jacek.a.tomaszewski@allegro.pl>
2022-10-11 09:14:35 +0200

    Generate library using `mix new`

---
+++ lib/ecto_cursor_based_stream.ex
@@ -0,0 +1,18 @@
+defmodule EctoCursorBasedStream do
+  @moduledoc """
+  Documentation for `EctoCursorBasedStream`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoCursorBasedStream.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_cursor_based_stream_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoCursorBasedStreamTest do
+  use ExUnit.Case
+  doctest EctoCursorBasedStream
+
+  test "greets the world" do
+    assert EctoCursorBasedStream.hello() == :world
+  end
+end
