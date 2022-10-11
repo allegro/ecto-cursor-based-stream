96b6ef7d5fe7c99523f0e484ad1733d96a104b36
Jacek Tomaszewski <jacek.a.tomaszewski@allegro.pl>
2022-10-11 15:47:10 +0200

    feat: Add `cursor_based_stream/2` function

--- lib/ecto_cursor_based_stream.ex
+++ lib/ecto_cursor_based_stream.ex
@@ -1,18 +1,91 @@
 defmodule EctoCursorBasedStream do
   @moduledoc """
-  Documentation for `EctoCursorBasedStream`.
+  Use this module in any module that uses `Ecto.Repo`,
+  to enrich it with `cursor_based_stream\2` function.
+
+  Example:
+
+      defmodule MyRepo do
+        use Ecto.Repo
+        use EctoCursorBasedStream
+      end
   """
 
-  @doc """
-  Hello world.
+  defmacro __using__(_) do
+    quote do
+      @type cursor_based_stream_opts :: [
+              {:chunk_size, integer()}
+              | {:cursor, String.t() | integer()}
+              | {:cursor_field, atom()}
+            ]
 
-  ## Examples
+      @doc """
+      Returns a lazy enumerable that emits all entries from the data store
+      matching the given query.
 
-      iex> EctoCursorBasedStream.hello()
-      :world
+      In contrast to `Ecto.Repo.stream/2`,
+      this will not use database mechanisms (e.g. database transactions) to stream the rows.
 
-  """
-  def hello do
-    :world
+      It does so by sorting all the rows by `options[:cursor_field]`
+      and iterating over them in chunks of size `options[:max_rows]`.
+
+      ## Options
+
+      * `:cursor_field` - the field by which all rows should be iterated.
+
+      **This field must have unique values. (Otherwise, some rows may get skipped.)**
+
+      For performance reasons, we recommend that you have an index on that field. Defaults to `:id`.
+
+      * `:after_cursor` - the value of the `:cursor_field` must be greater than it.
+      Useful when you want to continue streaming from a certain point.
+      Any rows with value equal or smaller than this value will not be included.
+
+      Defaults to `nil`. (All rows will be included.)
+
+      * `:max_rows` - The number of rows to load from the database as we stream.
+      Defaults to 500.
+
+      ## Example
+
+          # Iterate over all posts
+          Post
+          |> MyRepo.cursor_based_stream(chunk_size: 100)
+          |> Stream.each(...)
+          |> Stream.run()
+      """
+      @spec cursor_based_stream(Ecto.Queryable.t(), cursor_based_stream_opts) ::
+              Enum.t()
+      def cursor_based_stream(queryable, options \\ []) do
+        import Ecto.Query
+
+        max_rows = Keyword.get(options, :max_rows, 500)
+        after_cursor = Keyword.get(options, :after_cursor)
+        cursor_field = Keyword.get(options, :cursor_field, :id)
+
+        Stream.unfold(after_cursor, fn cursor ->
+          rows =
+            queryable
+            |> order_by([o], asc: ^cursor_field)
+            |> then(fn q ->
+              if cursor do
+                q |> where([r], field(r, ^cursor_field) > ^cursor)
+              else
+                q
+              end
+            end)
+            |> limit(^max_rows)
+            |> all()
+
+          if Enum.empty?(rows) do
+            nil
+          else
+            next_cursor = rows |> List.last() |> Map.fetch!(cursor_field)
+            {rows, next_cursor}
+          end
+        end)
+        |> Stream.flat_map(& &1)
+      end
+    end
   end
 end
--- test/ecto_cursor_based_stream_test.exs
+++ test/ecto_cursor_based_stream_test.exs
@@ -1,8 +1,64 @@
 defmodule EctoCursorBasedStreamTest do
-  use ExUnit.Case
-  doctest EctoCursorBasedStream
+  use TestApp.RepoCase
 
-  test "greets the world" do
-    assert EctoCursorBasedStream.hello() == :world
+  describe "cursor_based_stream/2" do
+    setup do
+      rows = [
+        Repo.insert!(%User{email: "1@test.com"}),
+        Repo.insert!(%User{email: "2@test.com"}),
+        Repo.insert!(%User{email: "3@test.com"})
+      ]
+
+      %{rows: rows}
+    end
+
+    test "if rows total count is smaller than :max_rows option, streams all the rows", %{
+      rows: rows
+    } do
+      result = User |> Repo.cursor_based_stream(max_rows: 50) |> Enum.to_list()
+
+      assert result == rows |> Enum.sort_by(& &1.id)
+    end
+
+    test "if rows total count is equal to the :max_rows option, streams all the rows", %{
+      rows: rows
+    } do
+      result = User |> Repo.cursor_based_stream(max_rows: 3) |> Enum.to_list()
+
+      assert result == rows |> Enum.sort_by(& &1.id)
+    end
+
+    test "if rows total count is larger than the :max_rows option, streams all the rows", %{
+      rows: rows
+    } do
+      result = User |> Repo.cursor_based_stream(max_rows: 2) |> Enum.to_list()
+
+      assert result == rows |> Enum.sort_by(& &1.id)
+    end
+
+    test "if :cursor_field option is given, iterates rows over that field", %{rows: rows} do
+      result =
+        User
+        |> Repo.cursor_based_stream(max_rows: 2, cursor_field: :email)
+        |> Enum.to_list()
+
+      assert result == rows
+    end
+
+    test "if :after_cursor option is given, skips any rows with value not greater than it",
+         %{
+           rows: rows
+         } do
+      result =
+        User
+        |> Repo.cursor_based_stream(
+          max_rows: 2,
+          cursor_field: :email,
+          after_cursor: "1@test.com"
+        )
+        |> Enum.to_list()
+
+      assert result == rows |> Enum.slice(1, 2)
+    end
   end
 end
